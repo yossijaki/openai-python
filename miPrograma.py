456083dc77a9fd1c7d07b6ad128ca20cb7b9acb3
Yossijaki <yossijaki@gmail.com>
2024-05-26 17:00:37 -0600

    first commit

---
+++ miPrograma.py
@@ -0,0 +1,10 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+modelos=client.models.list()
+
+print(modelos)
