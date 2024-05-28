1705bed6d0d1c5cd5fb132604a782f61cf398cb8
Yossijaki <yossijaki@gmail.com>
2024-05-27 18:37:26 -0600

    changes to call the new version of the gpt api

--- miPrograma.py
+++ miPrograma.py
@@ -1,10 +1,17 @@
+from dotenv import load_dotenv
 import os
 from openai import OpenAI
-from dotenv import load_dotenv
 
 load_dotenv()
 
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
-modelos=client.models.list()
 
-print(modelos)
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-3.5-turbo",
+)
